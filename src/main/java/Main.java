511ea0e62fef8afd3af54055b5af2660e6e9ce73
Yackov <yackov.irkyt@gmail.com>
2023-05-10 20:34:55 +0500

    Ready push

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,16 @@
 public class Main {
+    public static void main(String[] args) {
+        Post post = new Post();
+        post.name = "Иван";
+        post.pasport = "1234 № 12345678";
+        post.patronymic = "Александрович";
+        post.phone = "+7(123)-456-78-90";
+        post.surname = "Петров";
+        post.subscription = true;
+
+        post.birthday = new FormDate();
+        post.birthday.day = 25;
+        post.birthday.month = 9;
+        post.birthday.year= 1998;
+    }
 }
--- src/main/java/Post.java
+++ src/main/java/Post.java
@@ -1,3 +1,11 @@
 public class Post {
-    
+    public String name; // поле для имени// добавьте другие поля
+    public String pasport;
+    public String patronymic;
+    public String phone;
+    public String surname;
+    public boolean subscription;
+    public FormDate birthday;
+    // заполните другие поля даты рождения
+
 }
